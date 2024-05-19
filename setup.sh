833cf6f4757d840c9fad6da568cad26082507a0b
James Zhou <jameszhou02@gmail.com>
2024-05-18 22:23:58 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get install net-tools
 sudo apt-get install smbclient
 sudo apt-get install cifs-utils
-runpodctl config --apiKey CZIZ7HIRD8WP96NLMXVWHUF612RLYKSOJBR3YT4S
+runpodctl config --apiKey QB36FXUTFOC314A1NRS8KQPHK9SBJA8AEQEYQ8LD
 # sudo mkdir /mnt/training-data
